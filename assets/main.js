18dba52c62b2ecb621fe02c5c277bd6f6eb55bb9
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-01-11 11:14:06 +0800

    Added website assets and html files

---
+++ assets/main.js
@@ -0,0 +1,137 @@
+var items = [
+    {
+        'name': 'Jordan Peak Essential Beanie',
+        'price': 1165.50,
+        'image': 'assets/accessories-1.webp',
+        'category': 'Accessories',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Nike Everyday Cushioned Crew Socks (3-Pack)",
+        'price': 945.00,
+        'image': 'assets/accessories-2.webp',
+        'category': 'Accessories',
+        'brand': 'Nike'
+    },
+    {
+        'name': 'Nike Elite Mid Basketball Socks',
+        'price': 645.00,
+        'image': 'assets/accessories-3.webp',
+        'category': 'Accessories',
+        'brand': 'Nike'
+    },
+    {
+        'name': 'New Era 2-Tone Varsity NBA Pack Los Angeles Lakers 9Forty Snapback Cap',
+        'price': 2495.00,
+        'image': 'assets/accessories-4.webp',
+        'category': 'Accessories',
+        'brand': 'New Era'
+    },
+    {
+        'name': "Air Jordan 1 Retro High TD 'Royal Reimagined'",
+        'price': 3345.00,
+        'image': 'assets/youth-1.webp',
+        'category': 'Youth',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Under Armour Curry 11 GS 'Dub Nation'",
+        'price': 6795.00,
+        'image': 'assets/youth-2.webp',
+        'category': 'Youth',
+        'brand': 'Under Armour'
+    },
+    {
+        'name': "Jordan Tatum 1 GS 'Wave Runner'",
+        'price': 3730.50,
+        'image': 'assets/youth-3.webp',
+        'category': 'Youth',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Jordan Luka 2 GS 'Trick Shot'",
+        'price': 4225.50,
+        'image': 'assets/youth-4.webp',
+        'category': 'Youth',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Wmns Nike Dunk Low LX 'Light Smoke Grey and Photon Dust'",
+        "price": 6655.50,
+        "image": 'assets/womens-2.webp',
+        'category': 'Women',
+        'brand': 'Nike'
+    },
+    {
+        'name': "Wmns Air Jordan 1 Low SE 'Sail and Black'",
+        'price': 7095.00,
+        'image': 'assets/womens-1.webp',
+        'category': 'Women',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Nike Ja 1 EP 'Christmas'",
+        'price': 6895.00,
+        'id': 'Christmas',
+        'image': 'assets/new-arrival-2.webp',
+        'category': 'Men',
+        'brand': 'Nike'
+    },
+    {
+        'name': "Air Jordan 1 Retro High OG 'Yellow Ochre'",
+        'price': 9895.00,
+        'id': 'YellowOchre',
+        'image': 'assets/new-arrival-1.webp',
+        'category': 'Men',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Nike Air Alpha Force '88 'Light Bone and Coconut Milk'",
+        'price': 4437.00,
+        'id': 'LightBoneAndCoconutMilk',
+        'image': 'assets/special-sale-3.webp',
+        'category': 'Men',
+        'brand': 'Nike'
+    },
+    {
+        'name': "Puma MB.03 x Dexter's Laboratory",
+        'price': 6895.00,
+        'id': 'DextersLab',
+        'image': 'assets/new-arrival-3.webp',
+        'category': 'Men',
+        'brand': 'Puma'
+    },
+    {
+        'name': "Air Jordan 1 Low 'Royal Toe'",
+        'price': 6195.00,
+        'id': 'RoyalToe',
+        'image': 'assets/new-arrival-4.webp',
+        'category': 'Men',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Wmns Air Jordan 1 Mid 'Black, Gym Red and College Grey'",
+        'price': 4257.00,
+        'id': 'BlackGymRedAndCollegeGrey',
+        'image': 'assets/special-sale-1.webp',
+        'category': 'Women',
+        'brand': 'Jordan'
+    },
+    {
+        'name': "Wmns Nike Dunk Low LX 'White and Coconut Milk'",
+        'price': 4137.00,
+        'id': 'WhiteAndCoconutMilk',
+        'image': 'assets/special-sale-2.webp',
+        'category': 'Women',
+        'brand': 'Nike'
+    },
+    
+    {
+        'name': "Wmns Nike Air Force 1 '07 Next Nature 'Summit White and Bronzine'",
+        'price': 3717.00,
+        'id': 'SummitWhiteAndBronzine',
+        'image': 'assets/special-sale-4.webp',
+        'category': 'Women',
+        'brand': 'Nike'
+    }
+]
